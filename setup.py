787239f008871a0b6e9a36155d2061107c8026ba
Masa Jow <wyan.github@futomen.net>
2016-02-16 11:40:49 -0800

    Adding setup.py to be installed via pip.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name = 'BlockEx',
+      version = '1.0',
+      description = 'Parsing text with blocks of regexes',
+      author = 'Masa Jow',
+      author_email = 'shigmas@gmail.com',
+      py_modules=['BoockEx'],
+)

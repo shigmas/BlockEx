de552b681e76ce6e4e8a44f907dec73a3dcfdcf0
Masa <wyan.git@futomen.net>
2017-04-24 09:01:13 -0700

    Checkin the second half of the change:  - deleted the old tests  - Updated config (.travis.yml and setup.py)

--- setup.py
+++ setup.py
@@ -2,13 +2,6 @@
 
 import BlockEx
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name = 'BlockEx',
-      version = '1.0',
-      description = 'Parsing text with blocks of regexes',
-      author = 'Masa Jow',
-      author_email = 'shigmas@gmail.com',
-      download_url="https://github.com/shigmas/BlockEx/tarball/master",
-      packages = ['BlockEx'],
-)
+setup(test_suite="tests")
